e98b0e3a7d5396aa46898ff3eb9265acbda45ae5
Esteban Ordano <esteban@ordano.com.ar>
2012-08-30 19:46:20 -0300

    Move to distutils instead of setup_tools

--- setup.py
+++ setup.py
@@ -1,28 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-setup(name='selenese',
-      version='0.0.1',
-      description="Python Selenese translator",
-      long_description=open('README').read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name             = 'PySelenese',
+    version          = '0.0.2',
+    description      = "Python Selenese translator",
+    license          = 'MIT License',
+    long_description = open('README').read(),
+    classifiers      = [
         'Intended Audience :: Developers',
-        ],
-      keywords='',
-      author='J-P Stacey',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=[],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        ],
-      entry_points="""
-      # -*- entry_points -*- 
-      """,
-      )
+    ],
+    author           = 'J-P Stacey',
+    author_email     = '',
+    mantainer        = 'Esteban Ordano',
+    mantainer_email  = 'eordano@gmail.com',
+    url              = 'https://github.com/jpstacey/PySelenese',
+)
